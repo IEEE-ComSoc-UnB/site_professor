3827dd2a9d148de8e3a1dde0fe127538ebf931a4
Pedro Blandim <pedro.blandim@gmail.com>
2020-01-15 03:50:11 -0300

    login pronto

--- site_login/account/forms.py
+++ site_login/account/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.forms.widgets import PasswordInput, TextInput # criar form de login
 from blog.models import Usuario
 
 class ResgistroDeUsuario(UserCreationForm):
@@ -25,3 +26,7 @@ class AtualizarPerfil(forms.ModelForm):
     class Meta:
         model = Usuario
         fields = ['nome','idade','genero','escolaridade','curso','nacionalidade']
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput(attrs={'class':'validate','placeholder': 'Nome de Usuário'}))
+    password = forms.CharField(widget=PasswordInput(attrs={'placeholder':'Senha'}))
--- site_login/account/urls.py
+++ site_login/account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
 from . import views
 from django.contrib.auth.views import LoginView,LogoutView
+from .forms import LoginForm
 
 app_name = 'account'
 
@@ -8,7 +9,7 @@ urlpatterns = [
     path('registrar/', views.register, name='register'),
 
     # mudar a variável template_name de LoginView de registration/login.html para account/login.html
-    path('login/', LoginView.as_view(template_name="account/login.html", redirect_authenticated_user=True), name='login'),
+    path('login/', LoginView.as_view(template_name="account/login.html", redirect_authenticated_user=True,authentication_form=LoginForm), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('perfil/', views.perfilVisualizar, name='perfilVisualizar'),
     path('perfil/atualizar', views.perfilAtualizar, name='perfilAtualizar')
