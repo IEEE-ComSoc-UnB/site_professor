100a1cf1a9aad79a77ef8f4558f6e2c2d75122a5
Pedro Blandim <pedro.blandim@gmail.com>
2020-01-16 06:39:20 -0300

    resposta e permissão de aceessos aos formulários prontas

--- site_login/blog/views.py
+++ site_login/blog/views.py
@@ -21,6 +21,15 @@ def forms(request):
     usuario_logado = usuario_logado.usuario                         #usuario logado
     perfil = usuario_logado.perfil_especifico                       #perfil do usuario logado
     forms_available = perfil.formularios.order_by('-data_inicial')  #formularios do perfil
+    
+    if len(forms_available) == len(perfil.formularios.all()):
+        
+        # pag_anterior = request.META.HTTP_REFERER
+        # context = {
+        #     'pag_anterior': pag_anterior
+        # }
+        return render(request, 'blog/forms_respondidos.html', {})
+
     context = {'forms_available' : forms_available}
     return render(request, 'blog/forms.html', context)
 
@@ -38,6 +47,7 @@ def formulario(request, form_id):
 def pergunta(request, form_id, pergunta_num):
 
     formulario = Formulario.objects.get(pk=form_id)
+
     try:
         pergunta = formulario.perguntas.all()[pergunta_num]
         context = {
@@ -46,11 +56,12 @@ def pergunta(request, form_id, pergunta_num):
             'pergunta_num': pergunta_num
         }
     except:
-        # acabaram as perguntas
+        # formulario respondido
+        request.user.usuario.formularios.add(formulario)
         return redirect('/')
     return render(request, 'blog/pergunta.html', context)
 
-
+@login_required
 def resposta(request, form_id, pergunta_num):
 
     pub_data    = timezone.now().date()
@@ -71,6 +82,8 @@ def resposta(request, form_id, pergunta_num):
     alternativa = Alternativa.objects.get(pk=alternativa_id)
 
     Resposta.objects.create(pub_data=pub_data, usuario=usuario, formulario=formulario, pergunta=pergunta, alternativa=alternativa)
-    return HttpResponse('Resposta criada')
+    pergunta_num += 1
+    url = '/formulario/' + str(form_id) +'/pergunta/' + str(pergunta_num)
+    return redirect(url)
 
 
